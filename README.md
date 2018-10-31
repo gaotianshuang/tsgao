192c399d13f6acc349509b549f4ca614e2288bb2
gaotianshuang <gueen0@qq.com>
2018-10-31 20:40:11 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # tsgao
-个人站点
+个人网站
+
+## 此站点是借鉴的[@ Cong Min]大神
+
+* http://resume.congm.in
+
+Fork或借鉴请注明出处 [@ Cong Min](https://congm.in) . Thx
+
+前提: 已安装`nodejs` `npm`
+- 0、`npm i` 安装依赖
+- 1、**开发**：命令行运行 `npm run dev` 开启调试服务器
+- 2、编辑 `/src/index.html` 文件实时调试
+- 3、**生产**：命令行运行 `npm run build` 进行打包
+- 4、浏览器打开 `/index.html` 进行预览
+
+> chrome浏览器 - "打印" - 设置无边距 - 即可导出
---
+++ static/js/home.js
@@ -0,0 +1,77 @@
+var iUp = (function() {
+    var t = 0,
+        d = 150,
+        clean = function() {
+          t = 0;
+        },
+        up = function(e) {
+          setTimeout(function() {
+            $(e).addClass("up")
+          }, t);
+          t += d;
+        },
+        down = function(e) {
+          $(e).removeClass("up");
+        },
+        toggle = function(e){
+          setTimeout(function() {
+            $(e).toggleClass("up")
+          }, t);
+          t += d;
+        };
+    return {
+      clean: clean,
+      up: up,
+      down: down,
+      toggle: toggle
+    }
+  })();
+  function xSlideIn() {
+    $('.btn-mobile-menu').removeClass('hidden');
+    $('.main-post-list').removeClass('hidden');
+    $('.panel-cover').addClass('panel-cover--collapsed');
+    $('.content-wrapper').addClass('animated slideInRight');
+  }
+  function xSlideOut() {
+    $('.btn-mobile-menu').addClass('hidden');
+    $('.main-post-list').addClass('hidden');
+    $('.panel-cover').removeClass('panel-cover--collapsed');
+    $('.content-wrapper').removeClass('animated slideInRight');
+  }
+  function ySlide() {
+    var wrapper = $('.navigation-wrapper');
+    if (wrapper.css('display') == "block") {
+      wrapper.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        wrapper.toggleClass('visible animated bounceOutUp');
+      });
+      wrapper.toggleClass('animated bounceInDown animated bounceOutUp');
+    } else {
+      wrapper.toggleClass('visible animated bounceInDown');
+    }
+    $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+  }
+  $(function() {
+    $(".navigation--btns .navigation__item:first a").addClass("index-button").attr('target', '_self');
+    // if(window.location.host == "i.congm.in") {
+    //   xSlideIn();
+    // }else{
+    //   xSlideOut();
+    // }
+    $(".iUp").each(function(i, e) {
+      iUp.up(e);
+    });
+    $(".btn-mobile-menu__icon").on("click", function(){
+      ySlide();
+    });
+    $(".index-button").on("click", function(){
+      if(window.location.host == "resume.mflywork.cn"){
+        xSlideOut();
+        setTimeout(function(){
+          //window.location.href = "http://resume.mflywork.cn/";
+        }, 600)
+      }
+    });
+  });
+  $(document.links).filter(function() {
+      return (this.hostname != window.location.hostname) && (this.target != '_self');
+  }).attr('target', '_blank');
